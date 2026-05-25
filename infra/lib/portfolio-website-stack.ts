26264fc6cc3ef9dcfd1dbcdd50dd6daf22e891d9
Matt Ignaczak <matthew.ignaczak@gentex.com>
2026-05-25 13:27:17 +0000

    added cert to hit this from the domain name

--- infra/lib/portfolio-website-stack.ts
+++ infra/lib/portfolio-website-stack.ts
@@ -5,6 +5,8 @@ import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import path from 'path';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PortfolioWebsiteStack extends cdk.Stack {
@@ -17,12 +19,21 @@ export class PortfolioWebsiteStack extends cdk.Stack {
       autoDeleteObjects: true,
     });
 
+    // TODO: add arn to the cdk config
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      'Certificate',
+      'arn:aws:acm:us-east-1:325056425651:certificate/0d673c7e-22fa-4a9b-9bea-7ebbe327c55d',
+    );
+    // TODO: move the domain names and default root to the cdk config.
     const distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
         origin: origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
       defaultRootObject: 'index.html',
+      domainNames: ['www.sandbox.mattignaczak.xyz', 'sandbox.mattignaczak.xyz'],
+      certificate,
     });
 
     new s3deploy.BucketDeployment(this, 'DeployWebsite', {
