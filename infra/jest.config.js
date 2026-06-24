f4a48805a9e4d0c8290e5acd1923ec7bd93fc4ac
Matt Ignaczak <matthew.ignaczak@gentex.com>
2026-06-24 01:56:59 +0000

    moved stack vars into .env + cdk config, added infra tests

--- infra/jest.config.js
+++ infra/jest.config.js
@@ -5,5 +5,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
+  // Runs before test modules load — sets DEPLOY_ENV so cdk-config.ts doesn't throw.
+  setupFiles: ['<rootDir>/test/setup.ts'],
   setupFilesAfterEnv: ['aws-cdk-lib/testhelpers/jest-autoclean'],
 };
